957e16db31d62f7be070225a20d75fdf56e1dfd2
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-05-12 22:39:27 +0100

    More on blue_dissemination

--- dissemination/include/blue_dissemination.h
+++ dissemination/include/blue_dissemination.h
@@ -1,16 +1,25 @@
 #ifndef BLUE_DISSEMINATION_H
 #define BLUE_DISSEMINATION_H
-#endif
+
+typedef struct blue_diss {
+    void** in_flight; // messages we're still periodically broadcasting
+    int*   seen;      // hashes of messages we've seen recently
+} blue_diss_t;
 
 struct Config {
 
 };
 
-int init(struct Config);
+blue_diss_t* init(struct Config);
 
-int broadcast(void* msg);
+int broadcast(blue_diss_t*, void* msg);
 
-int receive(void** msg, int* size);
+int receive(blue_diss_t*, void* msg, int count);
 
 
+// Stubs
 
+int blue_find(in_addr_t*, int);
+int blue_send(in_addr_t*, void*);
+
+#endif
--- dissemination/src/blue_dissemination.c
+++ dissemination/src/blue_dissemination.c
@@ -1,18 +1,31 @@
+#include <arpa/inet.h>
 #include <blue_dissemination.h>
 
-int init(struct Config cfg) {
+#define hashsize(n) ((uint64_t)1<<(n))
+#define hashmask(n) (hashsize(n)-1)
+
+blue_diss_t* init(struct Config cfg) {
+    blue_diss* bd = malloc(blue_diss);
+    return bd;
+}
+
+int broadcast(blue_diss_t* bd, void* msg) {
+
+    int size;
+    in_addr_t* addrs;
+
+    if (blue_find(&addrs, &size))
+        return 2;
+
+    for (int i = 0; i < size; i++)
+        blue_send(addrs[i], msg);
+
+    hashmask(msg);
 
     return 0;
 }
 
-int broadcast(void* msg) {
-
-
+int receive(blue_diss* bd, void** msg, int* size) {
     return 0;
 }
-
-int receive(void** msg, int* size) {
-    return 0;
-}
-
 
