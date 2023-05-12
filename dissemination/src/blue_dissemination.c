b4b1cc642afeb7373a209e7f20e6f00db339928d
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-05-12 22:07:14 +0100

    dissemination init

---
+++ dissemination/src/blue_dissemination.c
@@ -0,0 +1,18 @@
+#include <blue_dissemination.h>
+
+int init(struct Config cfg) {
+
+    return 0;
+}
+
+int broadcast(void* msg) {
+
+
+    return 0;
+}
+
+int receive(void** msg, int* size) {
+    return 0;
+}
+
+
