ed3f942c44942d92b422a9239c6c5af6d53ee185
tometom <tombsantos@gmail.com>
2023-05-12 23:20:19 +0100

    Corrected interface usage on app

--- app/src/main.c
+++ app/src/main.c
@@ -2,9 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "../../dissemination/include/blue_dissemination.h"
 
 extern char *optarg;
 extern int optind, opterr, optopt;
+size_t bufSize = 1024;
+int peerCycle(blue_diss_t* diss){
+    while(1){
+        msg_t* toRedirect = alloc_msg(bufSize);
+        int err = receive(diss,toRedirect);
+        if(err){
+            fprint(stderr,"Error occurred while receiving msg\n");
+            break;
+        }
+        printf("Message received: %s\n",toRedirect->buf);
+    }
+}
+int castCycle(char* msg){
+}
 int main(int argc, char *argv[])
 {
     if (argc == 1)
@@ -22,11 +37,11 @@ int main(int argc, char *argv[])
         {
         case 'p':
             pflag = 1;
-            printf("pflag up\n");
+            printf("Acting as peer.\n");
             break;
         case 'b':
             msg = optarg;
-            printf("%s\n",msg);
+            printf("Broadcasting %s\n",msg);
             break;
         case '?':
             if (optopt == 'b')
@@ -41,5 +56,18 @@ int main(int argc, char *argv[])
         default:
             abort();
         }
+    if(msg!=NULL && pflag==1){
+        fprintf(stderr,"Only one flag can be set. Either p or b\n");
+        return 1;
+    }
+    struct Config cfg;
+    blue_diss_t* diss = init(cfg);
+    if(pflag){
+        //Acting as peer
+        int resp = peerCycle(diss);
+    }else{
+        //Acting as broadcaster
+        int resp = castCycle(msg);
+    }
     return 0;
 }
