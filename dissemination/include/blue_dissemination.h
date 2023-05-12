0cd8af500c04800c88c9ff35195c5a1e234cdb12
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-05-12 23:03:20 +0100

    Compilation

--- dissemination/include/blue_dissemination.h
+++ dissemination/include/blue_dissemination.h
@@ -3,16 +3,20 @@
 
 #include <arpa/inet.h>
 
-typedef struct blue_diss {
-    msg in_flight[32]; // messages we're still periodically broadcasting
-    int count_in_flight;
-    int*   seen;      // hashes of messages we've seen recently
-} blue_diss_t;
+#define IN_FLIGHT_SIZE 32
 
 typedef struct msg {
     size_t size;
     char*  buf;
 } msg_t;
+
+typedef struct blue_diss {
+    // messages we're still periodically broadcasting
+    msg_t in_flight[IN_FLIGHT_SIZE];
+    int count_in_flight;
+    // hashes of messages we've seen recently
+    int* seen;
+} blue_diss_t;
 
 struct Config {
 
@@ -20,9 +24,9 @@ struct Config {
 
 blue_diss_t* init(struct Config);
 
-int broadcast(blue_diss_t*, char* msg);
+int broadcast(blue_diss_t*, msg_t msg);
 
-int receive(blue_diss_t*, char* msg, int count);
+int receive(blue_diss_t*, msg_t msg);
 
 msg_t * alloc_msg(size_t size);
 
@@ -30,7 +34,7 @@ void free_msg(msg_t * msg);
 
 // Stubs
 
-int blue_find(in_addr_t*, int);
-int blue_send(in_addr_t*, void*);
+int blue_find(in_addr_t*, size_t);
+int blue_send(in_addr_t, char*, size_t);
 
 #endif
--- dissemination/src/blue_dissemination.c
+++ dissemination/src/blue_dissemination.c
@@ -1,6 +1,7 @@
+#include <stdlib.h>
 #include <arpa/inet.h>
 #include <blue_dissemination.h>
-#include <blue_dissemination.h>
+#include <murmur3_hash.h>
 
 #include <stdlib.h>
 
@@ -8,29 +9,28 @@
 #define hashmask(n) (hashsize(n)-1)
 
 blue_diss_t* init(struct Config cfg) {
-    blue_diss_t* bd = malloc(blue_diss_t);
+    blue_diss_t* bd = malloc(sizeof(blue_diss_t));
     return bd;
 }
 
-int broadcast(blue_diss_t* bd, void* msg, size_t size) {
+int broadcast(blue_diss_t* bd, msg_t msg) {
 
-    int size;
-    in_addr_t* addrs;
+    in_addr_t addrs[10];
 
-    if (blue_find(&addrs, &size))
+    if (!blue_find(addrs, 10))
         return 2;
 
-    for (int i = 0; i < size; i++)
-        blue_send(addrs[i], msg);
+    for (int i = 0; i < 10; i++)
+        blue_send(addrs[i], msg.buf, msg.size);
 
-    int hash = MurmurHash3_x86_32(msg, size);
+    int hash = MurmurHash3_x86_32(msg.buf, msg.size);
     bd->seen[hashmask(hash)] = hash;
-    bd->in_flight
+    bd->in_flight[bd->count_in_flight++ % IN_FLIGHT_SIZE] = msg;
 
     return 0;
 }
 
-int receive(blue_diss_t* bd, void** msg, int* size) {
+int receive(blue_diss_t* bd, msg_t msg) {
     return 0;
 }
 
