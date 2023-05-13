5ce8d407efcbe5d6b3f4357842d7793b19fe154c
tometom <tombsantos@gmail.com>
2023-05-13 19:57:48 +0100

    very basic endpoint design done on js

--- chessClient.js
+++ chessClient.js
@@ -1,14 +1,30 @@
 //White is 0, Black is 1
 
-function get_board(){
+function get_board() {
+    fetch("/getBoard").then((data)=>{
+        if(data.status == 200){
+            parse_fen(data.body);
+        }else{
+            console.log("Error getting board");
+        }
+
+    })
 
 }
-function id(){
-    id = Math.random()
+function id() {
     //GET TEAM FROM SERVER
-    fetch("/")
+    postData("/join?id="+id,).then((data) => {
+        if (data.status == 200) {
+            if (data.body == 'white') {
+                team = 0;
+            } else {
+                team = 1;
+            }
+        } else {
+            console.log("Error assigning team. /join no success");
+        }
+    });
 }
-function vote(move,id){
-
+function vote(move, id) {
 }
 var team = 0;
