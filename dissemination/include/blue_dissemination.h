647bc0ef0fe5ce01c40c2d4695b6ed07461a9642
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-05-13 15:06:29 +0100

    Start listening and in_flight thread

--- dissemination/include/blue_dissemination.h
+++ dissemination/include/blue_dissemination.h
@@ -1,6 +1,7 @@
 #ifndef BLUE_DISSEMINATION_H
 #define BLUE_DISSEMINATION_H
 
+#include <pthread.h>
 #include <arpa/inet.h>
 
 #define IN_FLIGHT_SIZE 32
@@ -8,29 +9,39 @@
 typedef struct msg {
     size_t size;
     char*  buf;
+    int timestamp;
 } msg_t;
 
 typedef struct blue_diss {
+
     // messages we're still periodically broadcasting
-    msg_t in_flight[IN_FLIGHT_SIZE];
+    msg_t* in_flight[IN_FLIGHT_SIZE];
     int count_in_flight;
-    // hashes of messages we've seen recently
+    pthread_mutex_t in_flight_lock;
+
+    // hashes of messages we've seen recently (indexed by hashmask(hash)!)
     int* seen;
-} blue_diss_t;
+
+    // listening thread
+    pthread_t listening_thread;
+    pthread_t in_flight_thread;
+
+} * blue_diss_t;
 
 struct Config {
-
+    int period; // broadcast every x seconds
+    int dead_after; // dead after x seconds
 };
 
-blue_diss_t* init(struct Config);
+blue_diss_t init(struct Config);
 
-int broadcast(blue_diss_t*, msg_t * msg);
+int broadcast(blue_diss_t, msg_t* msg);
 
-int receive(blue_diss_t*, msg_t * msg);
+int receive(blue_diss_t, msg_t* msg);
 
-msg_t * alloc_msg(size_t size);
+msg_t* alloc_msg(size_t size);
 
-void free_msg(msg_t * msg);
+void free_msg(msg_t* msg);
 
 // Stubs
 
--- dissemination/src/blue_dissemination.c
+++ dissemination/src/blue_dissemination.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <blue_dissemination.h>
@@ -8,12 +9,76 @@
 #define hashsize(n) ((uint64_t)1<<(n))
 #define hashmask(n) (hashsize(n)-1)
 
-blue_diss_t* init(struct Config cfg) {
-    blue_diss_t* bd = malloc(sizeof(blue_diss_t));
+blue_diss_t init(struct Config cfg) {
+    blue_diss_t bd = malloc(sizeof(struct blue_diss));
+    bd->in_flight = malloc(sizeof(struct msg) * IN_FLIGHT_SIZE);
+    bd->count_in_flight = 0;
+
+    // TODO: Check for errors...
+    pthread_create(&bd->listening_thread, NULL, listen, (void*)bd);
+    pthread_create(&bd->in_flight_thread, NULL, in_flight, (void*)bd);
+
+    pthread_mutex_init(&bd->in_flight_lock, NULL);
+
     return bd;
 }
 
-int broadcast(blue_diss_t* bd, msg_t msg) {
+/*
+ * Listen to bluetooth connections and messages, if the message is new, trigger
+ * a deliver and move it to the in-flight buffer which is processed by the in_flight thread.
+ */
+void* listen(void* bd_v) {
+    blue_diss_t bd = (blue_diss_t)bd_v;
+}
+
+/*
+ * Broadcast messages from the in-flight periodically until their time to live
+ * is over.
+ */
+void* in_flight(void* bd_v) {
+    int i;
+    blue_diss_t bd = (blue_diss_t)bd_v;
+
+    while (1) {
+
+        // We copy all the messages pointers whose messages must be sent from the
+        // in_flight list; we're the ones responsible for deleting them, so we can
+        // be sure the're still alive.
+        //
+        // It might happen that if the in_flight_buffer is full, messages we're
+        // currently holding are dropped from the buffer.  That's why, at the end,
+        // we go over the buffer again, freeing the messages that were dropped from
+        // the buffer or those whose time has run out.
+
+
+        pthread_mutex_lock(bd->in_flight_lock);
+
+        int count_in_flight_copy = bd->count_in_flight;
+        msg_t* in_flight_copy[count_in_flight_copy];
+
+        for (i=0; i<count_in_flight_copy; i++) {
+            // TODO: Filter messages based on timestamps and modulos of things
+            in_flight_copy[i] = bd->in_flight[i];
+        }
+        pthread_mutex_unlock(bd->in_flight_lock);
+
+        for (i=0; i<count_in_flight_copy; i++) {
+            // We've filtered messages to send above, we send them all here.
+        }
+
+        // TODO We need to check if any has been overwritten, and otherwise decrease TTL?
+        // If things have been overwritten, or run out of time, we free them.
+    }
+}
+
+
+/* Broadcast a message:
+ * 
+ * This will send one message to all nearby devices. It does not broadcast the
+ * message more than once, unlike messages that are received by the nearby
+ * devices running the application (see receive)
+ */
+int broadcast(blue_diss_t bd, msg_t* msg) {
 
     in_addr_t addrs[10];
 
@@ -21,16 +86,22 @@ int broadcast(blue_diss_t* bd, msg_t msg) {
         return 2;
 
     for (int i = 0; i < 10; i++)
-        blue_send(addrs[i], msg.buf, msg.size);
+        blue_send(addrs[i], msg->buf, msg->size);
 
-    int hash = MurmurHash3_x86_32(msg.buf, msg.size);
+    int hash = MurmurHash3_x86_32(msg->buf, msg->size);
     bd->seen[hashmask(hash)] = hash;
     bd->in_flight[bd->count_in_flight++ % IN_FLIGHT_SIZE] = msg;
 
     return 0;
 }
 
-int receive(blue_diss_t* bd, msg_t msg) {
+/* Receive a message:
+ *
+ * This is a blocking call (in the spirit of recv). It blocks waiting for new
+ * messages and returns whenever a message is received from the
+ * blue_dissemination network.
+ */
+int receive(blue_diss_t bd, msg_t* msg, size_t length) {
     return 0;
 }
 
