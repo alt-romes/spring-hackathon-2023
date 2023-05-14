0481ba7f221a2255337c26d57dec83980f28d1d2
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2023-05-14 01:00:46 +0100

    api.js: regen

--- docs/api.js
+++ docs/api.js
@@ -3,7 +3,6 @@ var getBoard = function(onSuccess, onError) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', '/board', true);
   xhr.setRequestHeader('Accept', 'application/json');
-  //xhr.setRequestHeader('CORS','same-origin')
   xhr.onreadystatechange = function () {
     var res = null;
     if (xhr.readyState === 4) {
@@ -63,4 +62,25 @@ var postVote = function(body, onSuccess, onError) {
     }
   };
   xhr.send(JSON.stringify(body));
+};
+
+var postTimeleft = function(onSuccess, onError) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/timeleft', true);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
+    var res = null;
+    if (xhr.readyState === 4) {
+      if (xhr.status === 204 || xhr.status === 205) {
+        onSuccess();
+      } else if (xhr.status >= 200 && xhr.status < 300) {
+        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
+        if (res) onSuccess(res);
+      } else {
+        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
+        if (res) onError(res);
+      }
+    }
+  };
+  xhr.send(null);
 };
